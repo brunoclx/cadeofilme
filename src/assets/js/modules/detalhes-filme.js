1a2121019092d4dcfa384d330ba79c7bd58ab246
brunoclx <brunocalix@gmail.com>
2025-07-03 07:23:41 -0300

    Adicionei validação de erro caso a api do imdb não retorne nada. Adicionei também uma validação que verifica se o filme esta nos cinemas, além de verificar se existe titulos semelhantes e caso não exista o box não é exibido.

--- src/assets/js/modules/detalhes-filme.js
+++ src/assets/js/modules/detalhes-filme.js
@@ -8,7 +8,7 @@ const urlParams = new URLSearchParams(queryString); // separando os elementos da
 const termoPesquisar = urlParams.get('id'); // pegando apenas o id
 
 async function detalhesFilme(termoPesquisado) {
-    termoPesquisado = termoPesquisar;
+    termoPesquisado = Number(termoPesquisar);
 
     let erroBuscar = document.getElementById("erro-buscar");
     let informacoesPrincipais = document.getElementById("informacoes-principais");
@@ -37,6 +37,7 @@ async function detalhesFilme(termoPesquisado) {
     let elenco = document.getElementById("elenco");
     let diretor = document.getElementById("diretor");
 
+    let boxMaisOpcoes = document.getElementById("mais-opcoes");
     let maisOpcoes = document.getElementById("lista-mais-opcoes");
 
     try {  
@@ -52,8 +53,9 @@ async function detalhesFilme(termoPesquisado) {
             fetch(`https://api.themoviedb.org/3/movie/${termoPesquisado}?language=pt-BR`, options), // detalhes do filme 
             fetch(`https://api.themoviedb.org/3/movie/${termoPesquisado}/release_dates`, options), // data delançamento e classificação
             fetch(`https://api.themoviedb.org/3/movie/${termoPesquisado}/watch/providers`, options), // onde assistir
-            fetch(`https://api.themoviedb.org/3/movie//${termoPesquisado}/credits`, options), // creditos (atores e diretor)
-            fetch(`https://api.themoviedb.org/3/movie/${termoPesquisado}/recommendations?language=pt-BR`, options) // recomendações
+            fetch(`https://api.themoviedb.org/3/movie/${termoPesquisado}/credits`, options), // creditos (atores e diretor)
+            fetch(`https://api.themoviedb.org/3/movie/${termoPesquisado}/recommendations?language=pt-BR`, options), // recomendações
+            fetch('https://api.themoviedb.org/3/movie/now_playing?language=pt-BR&page=1&region=BR', options)
         ]);
 
         let dados = await RESPOSTA[0].json(); // // detalhes do filme
@@ -61,16 +63,19 @@ async function detalhesFilme(termoPesquisado) {
         let watchProviders = await RESPOSTA[2].json(); // onde assistir
         let elencoCreditos = await RESPOSTA[3].json(); // creditos (atores e diretor)
         let similar = await RESPOSTA[4].json(); // recomendações
+        let nowPlay = await RESPOSTA[5].json();
 
         const ENCONTROU_FILME = dados.success; // verificando o status do retorno se encontrou ou não
+        const FILME_ADULTO = dados.adult;
 
-        if(ENCONTROU_FILME == false){ // se não encontrou ele retorna false
+        if(ENCONTROU_FILME == false || FILME_ADULTO){ // se não encontrou ele retorna false
             erroBuscar.classList.remove("hidden");
         }else{ // se encontrou ele retorna undefined
             // se o filme existir, remove a classe hidden dos blocos de conteúdo
             informacoesPrincipais.classList.remove("hidden");
             ondeAssistir.classList.remove("hidden");
             blocoCreditos.classList.remove("hidden");
+            maisOpcoes.classList.remove("hidden");
 
             backdrop[0].style.backgroundImage = `url(https://image.tmdb.org/t/p/w780${dados.backdrop_path})`; // carrega o backdrop do filme
             tituloFilme.innerHTML = dados.title || dados.name; // carrega o titulo ou nome de acordo com o retorno da API
@@ -87,72 +92,81 @@ async function detalhesFilme(termoPesquisado) {
             apiImdb(idImdb, notaImdb); // passa via parametro para a função importada ApiImdb
 
             sinopse.innerHTML = `${dados.overview}`; // Exibe no campo sinopse o valor de dados.overview
-
-            let watchProvidersRetorno = watchProviders.results.BR; // pega aqui apenas os Serviços que estão na chave BR            
             
-            if (!watchProvidersRetorno){ // se a variavel que tem os serviços que estão na chave BR estiver vazia
-                let informarErro = document.createElement("p"); // cria um elemento p
-                informarErro.classList.add("text-xs", "text-white"); // adiciona essas classes
-                informarErro.innerText = "No momento não encontramos este filme em nenhum serviço de streaming."; // adiciona este texto
-                ondeAssistir.appendChild(informarErro);// adiciona o elemento p criado dentro da div ondeAssistir
-            }else{ // se tiver conteúdo
-                let conteudoServicos = document.createElement("div"); // cria um elemento div
-                conteudoServicos.classList.add("flex", "flex-col", "gap-3"); // adiciona essas classes
-                ondeAssistir.appendChild(conteudoServicos); // adiciona a div criada em OndeAssistir
+            if(!watchProviders){
+                console.log("Este filme não esta disponivel no Brasil ou em outos paises.");
+            }else{
+                let watchProvidersRetorno = watchProviders.results.BR; // pega aqui apenas os Serviços que estão na chave BR        
                 
-                const ordem = ["ads", "flatrate", "rent", "buy"]; // crio um array com a ordem que eu quero
+                if (!watchProvidersRetorno){ // se a variavel que tem os serviços que estão na chave BR estiver vazia
+                    let noCinema = nowPlay.results.find(item => item.id === Number(termoPesquisar));
+                    let informarErro = document.createElement("p"); // cria um elemento p
+                    informarErro.classList.add("text-xs", "text-white"); // adiciona essas classes
+                    if(noCinema){
+                        informarErro.innerText = "Somente nos cinemas."; // adiciona este texto
+                    }else{
+                        informarErro.innerText = ":( Poxa! Parece que este filme não está disponível em nenhum serviço nem nos cinemas agora."; // adiciona este texto
+                    }                    
+                    ondeAssistir.appendChild(informarErro);// adiciona o elemento p criado dentro da div ondeAssistir                    
+                }else{ // se tiver conteúdo
+                    let conteudoServicos = document.createElement("div"); // cria um elemento div
+                    conteudoServicos.classList.add("flex", "flex-col", "gap-3"); // adiciona essas classes
+                    ondeAssistir.appendChild(conteudoServicos); // adiciona a div criada em OndeAssistir
+                    
+                    const ordem = ["ads", "flatrate", "rent", "buy"]; // crio um array com a ordem que eu quero
 
-                let temTipo = []; //crio um array para contar os serviços exibidos
+                    let temTipo = []; //crio um array para contar os serviços exibidos
 
-                ordem.forEach(tipo => { // faço um forEach no array
-                    const servicos = watchProvidersRetorno[tipo];
-                    if (!servicos || !servicos.length) return;
+                    ordem.forEach(tipo => { // faço um forEach no array
+                        const servicos = watchProvidersRetorno[tipo];
+                        if (!servicos || !servicos.length) return;
 
-                    switch (tipo) {
-                        case "ads":
-                            const servicosBoxTipoAds = criarBoxServico(conteudoServicos, "Grátis");
-                            temTipo.push(servicosBoxTipoAds);
-                            servicos.forEach(servico => {
-                                const elementoServico = criarServico(servico);
-                                servicosBoxTipoAds.appendChild(elementoServico);
-                            });
-                        break;
+                        switch (tipo) {
+                            case "ads":
+                                const servicosBoxTipoAds = criarBoxServico(conteudoServicos, "Grátis");
+                                temTipo.push(servicosBoxTipoAds);
+                                servicos.forEach(servico => {
+                                    const elementoServico = criarServico(servico);
+                                    servicosBoxTipoAds.appendChild(elementoServico);
+                                });
+                            break;
 
-                        case "flatrate":
-                            const servicosBoxTipoFlatrate = criarBoxServico(conteudoServicos, "Por Assinatura");
-                            temTipo.push(servicosBoxTipoFlatrate);
-                            servicos.forEach(servico => {
-                                const elementoServico = criarServico(servico);
-                                servicosBoxTipoFlatrate.appendChild(elementoServico);
-                            });
-                        break;
+                            case "flatrate":
+                                const servicosBoxTipoFlatrate = criarBoxServico(conteudoServicos, "Por Assinatura");
+                                temTipo.push(servicosBoxTipoFlatrate);
+                                servicos.forEach(servico => {
+                                    const elementoServico = criarServico(servico);
+                                    servicosBoxTipoFlatrate.appendChild(elementoServico);
+                                });
+                            break;
 
-                        case "rent":
-                            const servicosBoxTipoRent = criarBoxServico(conteudoServicos, "Alugar");
-                            temTipo.push(servicosBoxTipoRent);
-                            servicos.forEach(servico => {
-                                const elementoServico = criarServico(servico);
-                                servicosBoxTipoRent.appendChild(elementoServico);
-                            });
-                        break;
+                            case "rent":
+                                const servicosBoxTipoRent = criarBoxServico(conteudoServicos, "Alugar");
+                                temTipo.push(servicosBoxTipoRent);
+                                servicos.forEach(servico => {
+                                    const elementoServico = criarServico(servico);
+                                    servicosBoxTipoRent.appendChild(elementoServico);
+                                });
+                            break;
 
-                        case "buy":
-                            const servicosBoxTipoBuy = criarBoxServico(conteudoServicos, "Comprar");
-                            temTipo.push(servicosBoxTipoBuy);
-                            servicos.forEach(servico => {
-                                const elementoServico = criarServico(servico);
-                                servicosBoxTipoBuy.appendChild(elementoServico);
-                            });
-                        break;
-                    }
-                });
-
-                if (temTipo.length > 1) {
-                    temTipo.forEach((box, index) => {
-                       if (index < temTipo.length - 1) {
-                            box.classList.add("border-b", "border-darkcade", "pb-3");
-                        } 
+                            case "buy":
+                                const servicosBoxTipoBuy = criarBoxServico(conteudoServicos, "Comprar");
+                                temTipo.push(servicosBoxTipoBuy);
+                                servicos.forEach(servico => {
+                                    const elementoServico = criarServico(servico);
+                                    servicosBoxTipoBuy.appendChild(elementoServico);
+                                });
+                            break;
+                        }
                     });
+
+                    if (temTipo.length > 1) {
+                        temTipo.forEach((box, index) => {
+                        if (index < temTipo.length - 1) {
+                                box.classList.add("border-b", "border-darkcade", "pb-3");
+                            } 
+                        });
+                    }
                 }
                 
                 let contaGenero = 0;
@@ -189,20 +203,26 @@ async function detalhesFilme(termoPesquisado) {
                 diretor.innerHTML = `<a href="pessoa.html?id=${elencoDiretor.id}">${elencoDiretor.name}.</a>`;
 
                 let todosSimilar = similar.results;
-                let contaSimilar = 0;
-                todosSimilar.forEach(similar => {
+                console.log(todosSimilar);
+                if(todosSimilar.length == 0){
+                    boxMaisOpcoes.classList.add("hidden");
+                }else{
+                    boxMaisOpcoes.classList.remove("hidden");
+                    let contaSimilar = 0;
+                    todosSimilar.forEach(similar => {
                     if(contaSimilar <= 5){
-                        let novoSimilar = document.createElement("li");
-                        let linkNovoSimilar = document.createElement("a");
-                        let imagemNovoSimilar = document.createElement("img");
-                        linkNovoSimilar.setAttribute("href", `filme.html?id=${similar.id}`);
-                        imagemNovoSimilar.setAttribute("src", `https://media.themoviedb.org/t/p/w600_and_h900_bestv2/${similar.poster_path}`);
-                        maisOpcoes.appendChild(novoSimilar);
-                        novoSimilar.appendChild(linkNovoSimilar);
-                        linkNovoSimilar.appendChild(imagemNovoSimilar);
-                        contaSimilar++
-                    }
-                });
+                            let novoSimilar = document.createElement("li");
+                            let linkNovoSimilar = document.createElement("a");
+                            let imagemNovoSimilar = document.createElement("img");
+                            linkNovoSimilar.setAttribute("href", `filme.html?id=${similar.id}`);
+                            imagemNovoSimilar.setAttribute("src", `https://media.themoviedb.org/t/p/w600_and_h900_bestv2/${similar.poster_path}`);
+                            maisOpcoes.appendChild(novoSimilar);
+                            novoSimilar.appendChild(linkNovoSimilar);
+                            linkNovoSimilar.appendChild(imagemNovoSimilar);
+                            contaSimilar++
+                        }
+                    });
+                }
 
             }            
         } 
--- src/assets/js/modules/imdbApi.js
+++ src/assets/js/modules/imdbApi.js
@@ -2,10 +2,10 @@ export async function apiImdb(idFilme, elementoNota) {
     try {
         const RESPOSTAIMDB = await fetch(`https://rest.imdbapi.dev/v2/titles/${idFilme}`);
         const dadosImdb = await RESPOSTAIMDB.json();
-        if(dadosImdb.rating.aggregate_rating != ""){
+        if(!dadosImdb){
             elementoNota.innerHTML = `${dadosImdb.rating.aggregate_rating}/10`;  
         }else{
-            elementoNota.innerHTML = "N/D"; 
+            elementoNota.innerText = "N/D"; 
         }
         
     } catch (error) {
